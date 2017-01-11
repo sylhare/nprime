243387445d8531cd742bae7a456e2e1eaf6241bc
Sylhare <sylhareng@gmail.com>
2017-01-11 14:30:19 -0500

    Not suitable n will be handled by Exceptions
    For n < 2 or when n is not a number.
    Instead of adding an extra validation within

--- src/primes.py
+++ src/primes.py
@@ -4,17 +4,15 @@ Created on Fri Jan  6 15:16:46 2017
 
 @author: sylhare
 """
+
 def isPrime(n):
     """
-    Check if the number "n" is prime.
+    Check if the number "n" is prime, with n>=2.
     Returns a boolean.
         
     """
-    if (type(n) is not int or n<2  ):
-        return False
-        
-    else:    
-        for i in range (2, int(n**0.5+1)):
-            if(n%i==0):
-                return False
-        return True     
+    for i in range(2, int(n**0.5)+1):
+        if(n%i == 0):
+            return False
+            
+    return True  
