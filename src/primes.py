cb3a54a2ef27a3bb572640746d0db25d3dc83747
Sylhare <sylhareng@gmail.com>
2017-01-17 09:30:19 -0500

    Adding two new functions

--- src/primes.py
+++ src/primes.py
@@ -16,3 +16,38 @@ def isPrime(n):
             return False
             
     return True  
+
+def genPrimes(limit=0):
+    """
+    Generate a list of primes from 2 to a set limit
+    Returns a list of integer.
+        
+    """
+    primes=[2] 
+        
+    for n in range(3, limit+1):    
+        k=0      
+        while (primes[k] <= n**0.5 and n%primes[k] != 0):
+            k=k+1
+            
+    #if a number has no dividers, last prime[k] of loop is over n's squareroot   
+        if (n**0.5 < primes[k]): 
+            primes.append(n)  
+           
+    return primes
+
+def findPrimes(lower, upper, primeTest=isPrime):
+    """
+    Find the number of primes between lower and upper range.
+    It should be lower < upper with lower >= 2.
+    The function to find the primes can be changed, using isPrime() as default
+    Returns a list of integer.
+    
+    """
+    primes=[]
+    
+    for n in range(lower, upper+1):
+        if primeTest(n):
+            primes.append(n)
+            
+    return primes     
