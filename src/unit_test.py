3d2423d3326fec5cef10268a6e6f26b7249ed61c
sylhare <sylhareng@gmail.com>
2017-03-30 16:38:46 -0400

    Updated
    added the isUniform function

--- src/unit_test.py
+++ src/unit_test.py
@@ -6,6 +6,7 @@ Created on Tue Feb 14 18:34:49 2017
 
 """
 
+import pyprime as p
 
 #Carmichael number often trigger false positive for the fermat algorithm
 __carmichael = [561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341]
@@ -58,6 +59,7 @@ def ppTest(function):
         
     return status
     
+    
 def carmiTest(function):
     """
     Test the function on the carmichael numbers
@@ -75,6 +77,39 @@ def carmiTest(function):
     return status  
     
     
+def isUniform(lower=2, upper=1000):
+    """
+    For all the fonction of pyprime, check if they generate the same
+    prime numbers as the isPrime function
+        
+    Returns True or the results if False
+    """
+    results = {'ref2':[], 'fermat':[], 'millerRabin':[]}
+    ref1 = p.findPrimes(lower, upper)
+    ref2 = p.genPrimes(upper)
+    fermat = p.findPrimes(lower, upper, p.fermat)
+    millerRabin = p.findPrimes(lower, upper, p.millerRabin)
+    
+    #Testing if we have the same primes from ref1 in all other functions
+    for n in ref1:
+        if n not in ref2:
+            results['ref2'].append(n)
+        if n not in fermat:
+            results['fermat'].append(n)
+        if n not in millerRabin:
+            results['millerRabin'].append(n)
+
+    #Making sure there's more detected primes than in ref1
+    for key in results:
+        results[key].append(len(results[key]))
+        
+        if results[key] != [0]:
+            return results
+    
+    return True
+              
+    
+    
 #### Sub fonctions to create the tests ####
     
 def functionTest(function, src):
@@ -88,6 +123,7 @@ def functionTest(function, src):
         results.append((n, function(n)))
     
     return results
+    
 
 def passTest(results):
     """
