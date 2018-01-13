620580f5002b7e5470d31c00f7e132d44bfd31e1
sylhare <sylhareng@gmail.com>
2018-01-13 11:52:24 -0500

    Fixing #8 with setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Pyprime',
+      version='0.0.1',
+      description='Python library for primes',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(exclude=['app', 'tests*']),
+     )
