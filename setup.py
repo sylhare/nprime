e1dbf62fe5812a6bfda5f7640c72709ce56b1ec9
sylhare <sylhareng@gmail.com>
2018-01-14 11:44:05 -0500

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='Pyprime',
-      version='0.0.1',
+      version='0.1.1',
       description='Python library for primes',
-      author='',
-      author_email='',
-      url='',
+      author='sylhare',
+      author_email='sylhare@outlook.com',
+      url='https://github.com/Sylhare/PyPrime',
       packages=find_packages(),
      )
