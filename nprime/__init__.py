1f81dc2813826af866cb855d24cf568cba3759a5
joncrall <erotemic@gmail.com>
2020-05-19 17:19:14 -0400

    use mkinit with --nomods

--- nprime/__init__.py
+++ nprime/__init__.py
@@ -2,21 +2,17 @@
 nprime main application package
 
 Auto generate command:
-    mkinit nprime/__init__.py -w
+    mkinit nprime/__init__.py --nomods -w
 """
-
-from nprime import pyprime
-
-from nprime.pyprime import (fermat, find_primes, generate_primes, is_prime,
-                            miller_rabin, pyprime, sacks, sieve_eratosthenes,
-                            trial_division, ulam, )
-
-__all__ = ['fermat', 'find_primes', 'generate_primes', 'is_prime',
-           'miller_rabin', 'pyprime', 'pyprime', 'sacks',
-           'sieve_eratosthenes', 'trial_division', 'ulam', ]
-
 __submodules__ = [
     'pyprime',
 ]
 
 __version__ = '1.1.1'
+from nprime.pyprime import (fermat, find_primes, generate_primes, is_prime,
+                            miller_rabin, pyprime, sacks, sieve_eratosthenes,
+                            trial_division, ulam,)
+
+__all__ = ['fermat', 'find_primes', 'generate_primes', 'is_prime',
+           'miller_rabin', 'pyprime', 'sacks', 'sieve_eratosthenes',
+           'trial_division', 'ulam']
