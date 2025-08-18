b601bea36558018ce32642bff12bb385b9ae8f36
sylhare <sylhare@outlook.com>
2025-08-18 03:37:52 -0400

    Release v.1.3.1

--- nprime/__init__.py
+++ nprime/__init__.py
@@ -12,7 +12,7 @@ __submodules__ = [
     'pyprime',
 ]
 
-__version__ = '1.2.1'
+__version__ = '1.3.1'
 
 __all__ = ['fermat', 'find_primes', 'generate_primes', 'is_prime',
            'miller_rabin', 'pyprime', 'sacks', 'sieve_eratosthenes',
